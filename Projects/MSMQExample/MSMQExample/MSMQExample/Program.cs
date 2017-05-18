91bab797dbe0fab1c4db95e683f9b0b32e207238
PonnaSai <saiponna198@gmail.com>
2017-05-18 10:29:55 -0400

    Changes

---
+++ Projects/MSMQExample/MSMQExample/MSMQExample/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSMQExample
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            const string queueName = @".\private$\TestQueue";
+            SendMessageToQueue(queueName);
+
+            
+        }
+        public static void SendMessageToQueue( string queueName)
+        {
+            MessageQueue msMq = null;
+            if(!MessageQueue.Exists(queueName))
+            {
+                MessageQueue.Create(queueName);
+            }
+            else
+            {
+                msMq = new MessageQueue(queueName);
+            }
+
+            try
+            {
+                QueueNames qn = new QueueNames()
+                {
+                    firstName = "Sai",
+                    lastName = "Prakash"
+                };
+                msMq.Send(qn);
+            }
+            catch (MessageQueueException ee)
+            {
+                Console.Write(ee.ToString());
+            }
+            catch (Exception eee)
+            {
+                Console.WriteLine(eee.ToString());
+            }
+
+            finally
+            {
+                msMq.Close();
+            }
+            Console.WriteLine("Message sent......");
+        }
+
+    }
+
+    public class QueueNames
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+
+}
