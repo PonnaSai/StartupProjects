7f9715429cbc6065237f867b233c7a25c6a67557
PonnaSai <saiponna198@gmail.com>
2017-06-21 14:00:35 -0400

    SecondScript

---
+++ public/New folder/App_Start/FilterConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BalanceTransfer
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(new NoCacheGlobalActionFilter());
+        }
+    }
+
+    public class NoCacheGlobalActionFilter : ActionFilterAttribute
+    {
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            HttpCachePolicyBase cache = filterContext.HttpContext.Response.Cache;
+            cache.SetCacheability(HttpCacheability.NoCache);
+            cache.SetMaxAge(new System.TimeSpan(0, 0, 0));
+            cache.SetNoStore();
+
+            base.OnResultExecuted(filterContext);
+        }
+    }
+}
---
+++ public/New folder/Common/TrustAllCertsPolicy.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BalanceTransfer.Common
+{
+    public class TrustAllCertsPolicy
+    {
+        //This class handles problems with certificates if ssl (https) is used
+        public static bool ValidateCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            //always return true, we are not checking certs yet
+            return true;
+        }
+
+    }
+}
